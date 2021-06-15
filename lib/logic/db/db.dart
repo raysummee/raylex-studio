566d5e9525a1a268208088d2b69e7a65e9d77482
Angshuman Barpujari <angshuarin@gmail.com>
2021-06-15 22:45:14 +0530

    registering adapters

--- lib/logic/db/db.dart
+++ lib/logic/db/db.dart
@@ -1,15 +1,25 @@
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:raylex_studio/logic/helpers/modelRecordHelper.dart';
+import 'package:raylex_studio/logic/helpers/modelTrackHelper.dart';
+import 'package:raylex_studio/logic/models/modelRecord.dart';
+import 'package:raylex_studio/logic/models/modelTrack.dart';
 
 class Db{
   Future<void> init() async{
     await Hive.initFlutter();
+    registerAdapters();
     await openAllBox();
   }
 
   Future<void> openAllBox() async{
     await ModelRecordHelper().openBox();
+    await ModelTrackHelper().openBox();
+  }
+
+  void registerAdapters(){
+    Hive.registerAdapter(ModelTrackAdapter());
+    Hive.registerAdapter(ModelRecordAdapter());
   }
 
 }
