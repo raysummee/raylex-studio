d959d7f279005759dc6c17a65f06a2667c95ed0d
Angshuman Barpujari <angshuarin@gmail.com>
2021-06-15 22:47:08 +0530

    add method added to record helper

--- lib/logic/helpers/modelRecordHelper.dart
+++ lib/logic/helpers/modelRecordHelper.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/foundation.dart' show ValueListenable;
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart' show BoxX;
+import 'package:raylex_studio/logic/helpers/modelTrackHelper.dart';
 import 'package:raylex_studio/logic/models/modelRecord.dart';
+import 'package:raylex_studio/logic/models/modelTrack.dart';
 
 class ModelRecordHelper{
   Future<Box<ModelRecord>> openBox() async{
@@ -7,5 +11,43 @@ class ModelRecordHelper{
   }
   Box<ModelRecord> box(){
     return Hive.box<ModelRecord>("records");
+  }
+
+  ValueListenable<Box<ModelRecord>> listen(){
+    return box().listenable();
+  }
+
+  ModelRecord? getAt(int index){
+    return box().getAt(index);
+  }
+
+  int length(){
+    return box().length;
+  }
+
+  bool isEmpty(){
+    return box().isEmpty;
+  }
+
+  Future<void> add({
+    required String name,
+    List<ModelTrack>? tracks,
+    required ModelTrack previewTrack,
+    bool exported: false
+  }) async{
+    ModelRecord record = ModelRecord(
+      name: name, 
+      previewTrack: previewTrack, 
+      exported: exported, 
+      onCreated: DateTime.now(), 
+      onUpdated: DateTime.now()
+    );
+    await box().add(record);
+    if(tracks == null) return;
+    await ModelTrackHelper().addAll(tracks);
+  }
+
+  Future<void> update(ModelRecord record) async{
+    await record.save();
   }
 }
