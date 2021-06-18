abbb94fa8067608ff60370e48728cd443355d07a
Angshuman Barpujari <angshuarin@gmail.com>
2021-06-18 23:30:13 +0530

    lib record is added in each track

--- lib/logic/models/modelTrack.dart
+++ lib/logic/models/modelTrack.dart
@@ -1,4 +1,5 @@
 import 'package:hive/hive.dart';
+import 'package:raylex_studio/logic/lib/libRecord.dart';
 part 'modelTrack.g.dart';
 
 @HiveType(typeId: 0)
@@ -9,9 +10,11 @@ class ModelTrack{
   int path;
   @HiveField(2)
   double milis;
+  LibRecord? record;
   ModelTrack({
     required this.name,
     required this.path,
-    required this.milis
+    required this.milis,
+    this.record
   });
 }
