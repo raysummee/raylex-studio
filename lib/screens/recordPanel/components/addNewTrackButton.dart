d1972ede7a05dc49c946c9b0b76fad0b42031b64
Angshuman Barpujari <angshuarin@gmail.com>
2021-06-18 14:40:01 +0530

    add new track button has play button, and has elevation now

--- lib/screens/recordPanel/components/addNewTrackButton.dart
+++ lib/screens/recordPanel/components/addNewTrackButton.dart
@@ -8,33 +8,58 @@ class AddNewTrackButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.bottomRight,
-      child: Padding(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 10,
+              color: Colors.black.withOpacity(0.02),
+              offset: Offset(0, -3)
+            )
+          ]
+        ),
         padding: EdgeInsets.only(
           bottom: MediaQuery.of(context).viewPadding.bottom+8, 
-          right: 28
+          top: 8,
+          right: 28,
+          left: 28
         ),
-        child: CupertinoButton(
-          onPressed: (){},
-          padding: EdgeInsets.zero,
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                "Add New Track",
-                style: TextStyle(
-                  color: Color.fromARGB(255, 112, 112, 112),
-                  fontSize: 17,
-                  fontWeight: FontWeight.bold
-                ),
-              ),
-              SizedBox(width: 10,),
-              Icon(
-                Icons.add_circle,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            CupertinoButton(
+              child: Icon(
+                Icons.play_arrow,  
+                size: 30,                     
                 color: Color.fromARGB(255, 112, 112, 112),
-                size: 30,
-              )
-            ],
-          ),
+              ), 
+              onPressed: (){}
+            ),
+            CupertinoButton(
+              onPressed: (){},
+              padding: EdgeInsets.zero,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "Add New Track",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 112, 112, 112),
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  SizedBox(width: 10,),
+                  Icon(
+                    Icons.add_circle,
+                    color: Color.fromARGB(255, 112, 112, 112),
+                    size: 30,
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
