4cc19d3bc5bf829551c42127dbafb104d54182d2
Angshuman Barpujari <angshuarin@gmail.com>
2021-06-19 23:27:19 +0530

    requried args are fitted

--- lib/screens/recordPanel/recordPanel.dart
+++ lib/screens/recordPanel/recordPanel.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
 import 'package:chewie/chewie.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:raylex_studio/logic/enums/RecordTileType.dart';
+import 'package:raylex_studio/logic/helpers/modelRecordHelper.dart';
 import 'package:raylex_studio/logic/lib/libRecord.dart';
 import 'package:raylex_studio/logic/models/modelRecord.dart';
 import 'package:raylex_studio/logic/models/modelTrack.dart';
@@ -47,7 +50,7 @@ class _RecordPanelState extends State<RecordPanel> {
 
   @override
   void dispose() {
-    chewieController.dispose();
+    // chewieController.dispose();
     videoPlayerController.dispose();
     record.tracks!.forEach((track) {
       track.record!.dispose();
@@ -60,7 +63,7 @@ class _RecordPanelState extends State<RecordPanel> {
     if(widget.record==null){
       record = ModelRecord(
         name: "New Recording", 
-        tracks: [ModelTrack(name: "Track 1", path: 11, milis: 0, recordType: RecordTileType.Record)], 
+        tracks: [ModelTrack(name: "Track 1", path: "track_1", milis: 0, recordType: RecordTileType.Record)], 
         exported: false, 
         onCreated: DateTime.now(), 
         onUpdated: DateTime.now()
@@ -87,12 +90,24 @@ class _RecordPanelState extends State<RecordPanel> {
     // LibRecord.init();
     super.initState();
   }
+  Future<File> moveFile(File sourceFile, String newPath) async {
+    final newFile = await sourceFile.copy(newPath);
+    return newFile;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: PreferredSize(
-        child: RecordPanelAppbar(title: record.name, sliderValue: sliderValue,), 
+        child: RecordPanelAppbar(onEnd: () async{
+          var basNameWithExtension = record.tracks![0].path;
+          var source = File((await record.tracks![0].record!.url())!);
+          var dest = (await getApplicationDocumentsDirectory()).path + "/" + basNameWithExtension;
+          var file =  await moveFile(source,dest);
+          record.tracks![0].path = file.path;
+          await ModelRecordHelper().add(name: "New", previewTrack: record.tracks![0], tracks: record.tracks);
+          Navigator.of(context).pop();
+        },title: record.name, sliderValue: sliderValue,), 
         preferredSize: Size.fromHeight(85)
       ),
       body: Stack(
@@ -140,14 +155,14 @@ class _RecordPanelState extends State<RecordPanel> {
                 if(track.recordType==RecordTileType.Record){
                   play? track.record!.recordStart(): track.record!.stopRecorder();
                 }else{
-                  if(play) track.record!.play(() {checkAllTrackEnded();});
+                  play? track.record!.play(() {checkAllTrackEnded();}) : track.record!.stopPlayer();
                 }
               });
               return true;
             },
             addNewTrack: () async{
               print("Adding");
-              var track = ModelTrack(name: "New Track ${record.tracks!.length+1}", path: 112, milis: 0);
+              var track = ModelTrack(name: "New Track ${record.tracks!.length+1}", path: "new_track_${record.tracks!.length+1}", milis: 0);
               track.record = LibRecord();
               print("initing");
               await track.record!.init(track.name);
