83aca977f0ebd499809ecc7804dce597c7ce71b5
Angshuman Barpujari <angshuarin@gmail.com>
2021-06-19 23:42:54 +0530

    calling record controller

--- lib/screens/recordPanel/recordPanel.dart
+++ lib/screens/recordPanel/recordPanel.dart
@@ -1,11 +1,9 @@
-import 'dart:io';
 import 'package:chewie/chewie.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:raylex_studio/logic/controller/recordController.dart';
 import 'package:raylex_studio/logic/enums/RecordTileType.dart';
-import 'package:raylex_studio/logic/helpers/modelRecordHelper.dart';
 import 'package:raylex_studio/logic/lib/libRecord.dart';
 import 'package:raylex_studio/logic/models/modelRecord.dart';
 import 'package:raylex_studio/logic/models/modelTrack.dart';
@@ -27,19 +25,6 @@ class _RecordPanelState extends State<RecordPanel> {
   late ModelRecord record;
   late ChewieController chewieController;
   final videoPlayerController = VideoPlayerController.asset('assets/images/butterfly.mp4');
-
-  int endedTracks = 0;
-  void checkAllTrackEnded(){
-    print("deub");
-    if(record.tracks!.any((element) => element.recordType==RecordTileType.Record)){
-      return;
-    }
-    endedTracks++;
-    if(endedTracks>=record.tracks!.length){
-      record.onPlayStopDispatch!();
-      endedTracks = 0;
-    }
-  }
 
   void che(double val){
     print(val.toString());
@@ -90,22 +75,14 @@ class _RecordPanelState extends State<RecordPanel> {
     // LibRecord.init();
     super.initState();
   }
-  Future<File> moveFile(File sourceFile, String newPath) async {
-    final newFile = await sourceFile.copy(newPath);
-    return newFile;
-  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: PreferredSize(
         child: RecordPanelAppbar(onEnd: () async{
-          var basNameWithExtension = record.tracks![0].path;
-          var source = File((await record.tracks![0].record!.url())!);
-          var dest = (await getApplicationDocumentsDirectory()).path + "/" + basNameWithExtension;
-          var file =  await moveFile(source,dest);
-          record.tracks![0].path = file.path;
-          await ModelRecordHelper().add(name: "New", previewTrack: record.tracks![0], tracks: record.tracks);
+          await RecordController().saveRecording(record);
           Navigator.of(context).pop();
         },title: record.name, sliderValue: sliderValue,), 
         preferredSize: Size.fromHeight(85)
@@ -146,27 +123,16 @@ class _RecordPanelState extends State<RecordPanel> {
           ),
           AddNewTrackButton(
             onPlayClick: (play){
-              if(record.tracks!.where((track) => track.recordType==RecordTileType.Record).length>1){
-                print("one file can be recorded for now");
-                return false;
-              }
-              record.tracks!.forEach((track) {
-                print("tile type: ${track.recordType.toString()}");
-                if(track.recordType==RecordTileType.Record){
-                  play? track.record!.recordStart(): track.record!.stopRecorder();
-                }else{
-                  play? track.record!.play(() {checkAllTrackEnded();}) : track.record!.stopPlayer();
-                }
-              });
-              return true;
+              return RecordController().onPlaybuttonClicked(play, record);
             },
             addNewTrack: () async{
-              print("Adding");
-              var track = ModelTrack(name: "New Track ${record.tracks!.length+1}", path: "new_track_${record.tracks!.length+1}", milis: 0);
+              var track = ModelTrack(
+                name: "New Track ${record.tracks!.length+1}", 
+                path: "new_track_${record.tracks!.length+1}", 
+                milis: 0
+              );
               track.record = LibRecord();
-              print("initing");
               await track.record!.init(track.name);
-              print("added, state changing");
               setState(() {
                 record.tracks!.add(track);
               });
