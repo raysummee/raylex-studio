00074f7290132268281d78542680a239099df83a
Angshuman Barpujari <angshuairn@gmail.com>
2021-06-28 00:29:57 +0530

    FileType enum created

---
+++ lib/logic/enums/FileType.dart
@@ -0,0 +1,10 @@
+import 'package:hive/hive.dart';
+part 'FileType.g.dart';
+
+@HiveType(typeId: 3)
+enum FileType{
+  @HiveField(0)
+  audio,
+  @HiveField(2)
+  video
+}
---
+++ lib/logic/enums/FileType.g.dart
@@ -0,0 +1,46 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'FileType.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class FileTypeAdapter extends TypeAdapter<FileType> {
+  @override
+  final int typeId = 3;
+
+  @override
+  FileType read(BinaryReader reader) {
+    switch (reader.readByte()) {
+      case 0:
+        return FileType.audio;
+      case 2:
+        return FileType.video;
+      default:
+        return FileType.audio;
+    }
+  }
+
+  @override
+  void write(BinaryWriter writer, FileType obj) {
+    switch (obj) {
+      case FileType.audio:
+        writer.writeByte(0);
+        break;
+      case FileType.video:
+        writer.writeByte(2);
+        break;
+    }
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is FileTypeAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
