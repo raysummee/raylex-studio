25685c8d385374fd398d20ec60ac2d28585ccd32
Angshuman Barpujari <angshuarin@gmail.com>
2021-06-30 22:56:51 +0530

    saving dialog box created

--- lib/ux/screens/recordPanel/components/savingDialog.dart
+++ lib/ux/screens/recordPanel/components/savingDialog.dart
@@ -1,18 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:raylex_studio/logic/context/appContext.dart';
 
 class SavingDialog extends StatelessWidget {
   const SavingDialog({ Key? key }) : super(key: key);
-  
+  static GlobalKey<State>? _keyLoader;
+
+  static void show(){
+    hide();
+    _keyLoader = new GlobalKey<State>();
+    showDialog(
+      context: navigatorState.currentContext!,
+      barrierDismissible: false,
+      builder: (context) => SavingDialog()
+    );
+  }
+  static void hide(){
+    if(_keyLoader!=null
+        &&_keyLoader!.currentContext!=null
+        &&Navigator.of(_keyLoader!.currentContext!,rootNavigator: true).canPop()
+      ){
+      Navigator.of(_keyLoader!.currentContext!,rootNavigator: true).pop();
+    }
+    _keyLoader = null;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15)
+      ),
       child: Container(
+        padding: EdgeInsets.fromLTRB(0, 30, 0, 25),
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            CircularProgressIndicator(),
+            SizedBox(
+              height: 30,
+              width: 30,
+              child: CircularProgressIndicator(
+                color: Colors.red,
+              )
+            ),
+            SizedBox(height: 18,),
             Text(
-              "30%"
+              "Saving",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Color.fromRGBO(112, 112, 112, 1)
+              ),
             )
           ],
         ),
