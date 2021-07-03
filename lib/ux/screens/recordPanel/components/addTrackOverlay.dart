6448f2bfb866091d2ee2bebff74bb9465cac460a
Angshuman Barpujari <angshuarin@gmail.com>
2021-07-03 15:44:02 +0530

    added animation to track overlay

--- lib/ux/screens/recordPanel/components/addTrackOverlay.dart
+++ lib/ux/screens/recordPanel/components/addTrackOverlay.dart
@@ -1,29 +1,36 @@
 import 'package:flutter/material.dart';
 
 abstract class AddTrackOverlay{
-  static OverlayEntry createOverlayEntry(BuildContext context, LayerLink _layerLink) {
+  static OverlayEntry createOverlayEntry(BuildContext context, LayerLink _layerLink, AnimationController animation) {
     RenderBox renderBox = context.findRenderObject() as RenderBox;
     var size = renderBox.size;
     var offset = renderBox.localToGlobal(Offset(0, -10));
     print(offset.dy+size.height);
     return OverlayEntry(
-      builder: (context) => Positioned(
-        child: CompositedTransformFollower(
-          link: _layerLink,
-          offset: Offset(MediaQuery.of(context).size.width - 100 - 8, offset.dy - MediaQuery.of(context).size.height),
-          child: Material(
-            elevation: 4.0,
-            child: ListView(
-              padding: EdgeInsets.zero,
-              shrinkWrap: true,
-              children: <Widget>[
-                ListTile(
-                  title: Text('Syria'),
+      builder: (context) => AnimatedBuilder(
+        animation: animation,
+        builder:(context, child) =>  Positioned(
+          width: 100,
+          child: CompositedTransformFollower(
+            link: _layerLink,
+            offset: Offset(MediaQuery.of(context).size.width - 100 - 8, (offset.dy - MediaQuery.of(context).size.height)*animation.value.clamp(0, 1)),
+            child: Opacity(
+              opacity: (animation.value*animation.value).clamp(0, 1),
+              child: Material(
+                elevation: 4.0,
+                child: ListView(
+                  padding: EdgeInsets.zero,
+                  shrinkWrap: true,
+                  children: <Widget>[
+                    ListTile(
+                      title: Text('Syria'),
+                    ),
+                    ListTile(
+                      title: Text('Lebanon'),
+                    )
+                  ],
                 ),
-                ListTile(
-                  title: Text('Lebanon'),
-                )
-              ],
+              ),
             ),
           ),
         ),
